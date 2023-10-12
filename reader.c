fda934e846362622d36691a54a413237af3c44ff
MKekkar <mkekkar@ucsd.edu>
2023-10-12 02:05:10 -0700

    update starter skeleton

---
+++ reader.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_LINE_LEN 256
+#define STATS_FORMAT_STR "Number of lines: %d\nLongest field: %d characters\n"
+#define USAGE_FORMAT_STR "ERR: expected usage is %s -c <in_cols> [-d delimiter] [-s] col1 ...\n"
+
+int main(int argc, char **argv) {
+
+    int in_cols = 0;  /* number of columns in input */
+    int out_cols = 0; /* number of columns to output */
+
+    int c_flag = 0; /* keep track of whether or not we received the required c flag */
+
+    char delim = ','; /* keep track of the delimiter, by default it is a comma */
+
+    /* local variables to store the statistics */
+    int stats_flag = 0; /* keep track of whether -s flag is passed in or not */
+    unsigned int linecnt = 0;   /* number of lines in input */
+    unsigned int lngst_fld = 0; /* longest field in input */
+
+    /* TODO: process named command line options using getopt and store the information
+        in the appropriate local variables */
+    int c;
+    /* TODO: fill out this skeleton code calling getopt */
+    while ((c = getopt(argc, argv, "s" /* TODO: fix argument to getopt */)) != -1) {
+        switch (c) {
+        case 's':
+            stats_flag = 1;
+            break;
+        /* 
+        TODO: fill in remaining cases (c flag, d flag)
+        - You will want to use the optarg variable set by getopt, to access option args
+        - You will want to use atoi to convert strings to integer types
+        */
+        default:
+            fprintf(stderr, USAGE_FORMAT_STR, argv[0]);
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* TODO: uncomment following blok & check to make sure c argument was provided */
+    /* 
+    if ( -FILL IN- ) {
+        // we did not receive the required c option, print usage and terminate
+        fprintf(stderr, USAGE_FORMAT_STR, argv[0]);
+        return EXIT_FAILURE;
+
+    }
+    */
+
+    /* pointers to arrays which will be allocated dynamically by you */
+    char **buf = NULL; /* pointer to array of pointers to the start of
+                             each field in input buffer */
+    int *obuf = NULL;  /* pointer to array of output indices */
+
+    // allocate arrays dynamically
+    /* TODO: uncomment the block below & fill in mallocs */
+    /*
+    buf = malloc( (-FILL IN-) * sizeof(*buf) ); // should be based on in_cols
+    if (buf == NULL) { // always check if malloc failed
+        return EXIT_FAILURE;
+    }
+
+    obuf = malloc( (-FILL IN-) * sizeof(*obuf) ); // should be based on argc, optind
+    if (obuf == NULL) { // always check if malloc failed
+        free(buf);
+        return EXIT_FAILURE;
+    }
+    */
+
+    /* 
+    TODO: process other command line arguments (list of output columns) 
+    - Fill these into obuf!
+    */
+
+    char line[MAX_LINE_LEN]; /* read buffer for processing lines in input */
+    char *line_ptr;
+    /* TODO: using fgets, loop over input from stdin
+    steps:  read a line, 
+            parse the line as described in writeup, 
+            print target columns for this line
+            repeat for next line
+        repeat until no more lines 
+    */
+
+    /* TODO: use this line to print out the statistics if -s flag is set */
+    printf(STATS_FORMAT_STR, linecnt, lngst_fld);
+
+    // free the memory for arrays allocated with malloc 
+    /* TODO: uncomment these lines when you uncomment the mallocs above */
+    // free(buf); 
+    // free(obuf);
+    return EXIT_SUCCESS;
+}
